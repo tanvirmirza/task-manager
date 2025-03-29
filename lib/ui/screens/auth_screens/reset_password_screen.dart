e45700a76e4bd424f6aad8e0c8d35000dcc43e3b
tanvirmirza <mirzatanvir74@gmail.com>
2025-03-29 11:01:36 +0600

    Add Auth Screens

---
+++ lib/ui/screens/auth_screens/reset_password_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import '../../widgets/screen_background.dart';
+import 'login_screen.dart';
+
+class ResetPasswordScreen extends StatefulWidget {
+  const ResetPasswordScreen({super.key});
+
+  @override
+  State<ResetPasswordScreen> createState() => _ResetPasswordScreenState();
+}
+
+class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
+  final TextEditingController _newPasswordTEController = TextEditingController();
+  final TextEditingController _confirmNewpasswordTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ScreenBackground(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Text(
+                    'Set Password',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                   Text(
+                    'Set a new password minimum length of 6 letters.',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge
+                        ?.copyWith(color: Colors.grey),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  TextFormField(
+                    controller: _newPasswordTEController,
+                    textInputAction: TextInputAction.next,
+                    decoration: const InputDecoration(hintText: 'New Password'),
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    controller: _confirmNewpasswordTEController,
+                    textInputAction: TextInputAction.done,
+                    decoration: const InputDecoration(hintText: 'Confirm New Password'),
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Confirm')),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Center(
+                    child: RichText(
+                        text: TextSpan(
+                            style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.black54,
+                                fontWeight: FontWeight.w600),
+                            children: [
+                          const TextSpan(text: "Have account? "),
+                          TextSpan(
+                              text: "Sign In",
+                              style: const TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = _onTapSignInButton),
+                        ])),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+void _onTapSignInButton() {
+    Navigator.pushAndRemoveUntil(
+        context, MaterialPageRoute(builder: (context) => const LoginScreen()), (pre) => false);
+  }
+
+  @override
+  void dispose() {
+    _newPasswordTEController.dispose();
+    _confirmNewpasswordTEController.dispose();
+    super.dispose();
+  }
+}
---
+++ lib/ui/utils/assets_path.dart
@@ -0,0 +1,12 @@
+class AssetsPaths {
+  static const String _imagePath = 'assets/icons';
+  static const String backgroundSvg = '$_imagePath/background.svg';
+}
+
+
+// first back:#e9d5ef, frog: #823696 / #5e2173
+// second back: #f7d3e0 , frog: #bb4f70
+// third back:#fcf1d2 , frog:#efc873
+// forth back: #d6e8ea, frog: #6ea8a5
+
+  //Color(0xFF9C27B0)
